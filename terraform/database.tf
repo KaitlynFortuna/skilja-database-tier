3d958d8eaed62401fd43d9583c2a6d168abc39b8
Kaitlyn2022 <90858239+Kaitlyn2022@users.noreply.github.com>
2023-06-23 09:32:43 -0400

    build: add autoscaling groups template

--- terraform/database.tf
+++ terraform/database.tf
@@ -162,3 +162,24 @@ resource "aws_security_group" "allow_tls" {
     Name = "allow_tls"
   }
 }
+
+// Autoscaling security groups config
+// Website used: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/autoscaling_group
+
+resource "aws_launch_template" "foobar" {
+  name_prefix   = "foobar"
+  image_id      = "ami-1a2b3c"
+  instance_type = "t2.micro"
+}
+
+resource "aws_autoscaling_group" "bar" {
+  availability_zones = ["us-east-1a"]
+  desired_capacity   = 1
+  max_size           = 1
+  min_size           = 1
+
+  launch_template {
+    id      = aws_launch_template.foobar.id
+    version = "$Latest"
+  }
+}
