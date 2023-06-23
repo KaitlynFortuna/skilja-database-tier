12f6bc89d36c68c139b00009bbfa9ea2684696e4
Kaitlyn2022 <90858239+Kaitlyn2022@users.noreply.github.com>
2023-06-23 09:23:42 -0400

    build: add security group template

--- terraform/data.tf
+++ terraform/data.tf
@@ -1,1 +1,8 @@
 
+# Database Module
+# RDS Database
+# RDS Modules
+# Security Groups (monitor traffic going in and out) [reference Kamryn]
+# Make ports variables
+# Look into autoscalling groups (Something to think about)
+# Make sure connection to application (communication) is open [security groups]
--- terraform/database.tf
+++ terraform/database.tf
@@ -132,3 +132,33 @@ module "db" {
 
 #   # ... omitted
 # }
+
+// Security Groups Config
+// Site used: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/security_group
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
